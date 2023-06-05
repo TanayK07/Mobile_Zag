dcc3dc51e5c66cac638a205355a06c99e6b0b603
TanayK07 <tkedia7@gmail.com>
2023-06-05 19:15:47 +0530

    Migrating to react dom 5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Product from './Pages/Product';
 import Home from './Pages/Home';
 import './styles.css';
 import PaymentComponent from './Pages/Payments';
+import { Route,Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import '@fontsource/poppins';
 
-const history = createBrowserHistory();
+const theme = extendTheme({
+  fonts: {
+    body: 'Poppins, sans-serif',
+    heading: 'Poppins, sans-serif',
+  },
+});
+
+
 
 const App = () => {
   return (
-    <Router history={history}>
-      <Routes>
+    <>
+    {/* {/* // <Router history={history}> */}
+    <ChakraProvider theme={theme}>
+      <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
         <Route path="/payments" element={<PaymentComponent />} />
-      </Routes>
-    </Router>
+ </Routes>
+  </ChakraProvider>
+    {/* // </Router> */} 
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
-import '@fontsource/poppins';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-const theme = extendTheme({
-  fonts: {
-    body: 'Poppins, sans-serif',
-    heading: 'Poppins, sans-serif',
-  },
-});
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+    <ColorModeScript />
+    <App />
+    </BrowserRouter>
+  </StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
